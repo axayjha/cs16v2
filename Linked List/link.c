c52690bb6fb0c826c58798c6a6368c597614d7fb
Akshay Jha <akshayjha@live.in>
2016-08-08 01:26:40 +0530

    Update link.c

--- Linked List/link.c
+++ Linked List/link.c
@@ -1,4 +1,12 @@
-/*Linked List*/
+/*
+link.c
+Application of linked list using link.h header file
+
+
+Akshay
+CSE, 3rd sem
+
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,19 +17,18 @@
 
 int main()
 {
-	link *list = getNode(); //Initializes
+	struct link *list = getNode(); //Initializes
 
 	getInput(list); //Gets inputs
 
-	list = delete(list); //deletes last element
+	//list = delete(list); //deletes last element
 
-	append(list, 4); //adds 4 to the end
+	//append(list, 4); //adds 4 to the end
 	
-	printf("Length%d\n", len(list)); //prints length
-	
-	
+	printlen(list); //prints length
+	printindex(list, 5); //prints of index of 5
+
 	display(list); //prints whole linked list
-
-
+	print_sorted(list); //prints sorted list
 
 }
