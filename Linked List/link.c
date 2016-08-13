6af2e82980d7f37f616fac0893f3f6b4b3763050
Akshay Jha <akshayjha@live.in>
2016-08-13 20:54:34 +0530

    Update link.c

--- Linked List/link.c
+++ Linked List/link.c
@@ -8,29 +8,23 @@ CSE, 3rd sem
 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "link1.h"
+
+#include "link.h"
 
 
 
 
 int main()
 {
-	struct link *list = getNode(); //Initializes
-
-	getInput(list); //Gets inputs
-
-	//list = delete(list); //deletes last element
-
-	//append(list, 4); //adds 4 to the end
-	
-	printlen(list); //prints length
-	printindex(list, 5); //prints of index of 5
-	display(list);  //prints whole linked list
-	print_sorted(list); //prints sorted list
-
-	//display(reverse(list)); //prints reversed linked list
-	
+	struct link *list = new_list("3,4,5,6");
+	display(list);
 
 }
+
+
+ /*Output
+
+ { 3 4 5 6 }
+
+
+*/
